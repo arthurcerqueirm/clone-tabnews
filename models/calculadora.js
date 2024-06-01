0a4fdae48ef20370feffebcbdac7c5fb2a47140c
Arthur Cerqueira <92320774+arthurcerqueirm@users.noreply.github.com>
2024-06-01 20:41:01 +0000

    add path v1

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(numero1, numero2){
-  if (typeof numero1  !== "number" || typeof numero2 !== "number" )
-    return "Erro"
-  return parseFloat((numero1 + numero2).toFixed(3))
-
-}
-
-exports.somar = somar
