73bd2f86a861bc21acec4bae424678018451d5ed
Arthur Cerqueira <92320774+arthurcerqueirm@users.noreply.github.com>
2024-10-17 19:22:19 +0000

    finalizando endpoint /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js"
 
 async function status(request, response){
-  const result = await database.query("SELECT 1 + 1 as soma;")
-  console.log(result.rows)
-  response.status(200).json({chave : "valor"})
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionResultValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConectionsResult = await database.query("SHOW max_connections;")
+  const databaseMaxConectionsResultValue = databaseMaxConectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1`,
+    values: [databaseName]
+  });// SELECT count(*)::int from pg_stat_activity WHERE datname = 'local_db';
+  const databaseOpenedConectionsResultValue = databaseOpenedConectionsResult.rows[0].count;
+
+  response.status(200).json(
+    {
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersionResultValue,
+          max_connections: parseInt(databaseMaxConectionsResultValue),
+          opened_connections: databaseOpenedConectionsResultValue
+        }
+      }
+        
+    }
+  )
 }
 
 export default status
