2b3cfb25132f1eb6ee9a5731a2d5598c8457fb88
Arthur Cerqueira <92320774+arthurcerqueirm@users.noreply.github.com>
2024-10-29 18:10:11 +0000

    add's endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,20 @@
 import { Client } from "pg"
 
 async function query(queryObject){
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }finally{
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -10,24 +24,13 @@ async function query(queryObject){
     ssl: getSSLValue()
   })
 
-  
-  try {
-    await client.connect()
-    const result = await client.query(queryObject);
-    return result
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }finally{
-    await client.end();
-  }
-  
- 
-  
+  await client.connect()
+  return client
 }
 
 export default{
-  query: query,
+  query,
+  getNewClient,
 }
 
 function getSSLValue(){
@@ -37,5 +40,5 @@ function getSSLValue(){
       ca: process.env.POSTGRES_CA
     }
   }
-  return process.env.NODE_ENV === 'development' ? false : true
+  return process.env.NODE_ENV === 'development' || 'test' ? false : true
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,15 +1,16 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
 
 test("Teste de migrations", async ()=> {
   const response = await fetch("http:/localhost:3000/api/v1/migrations");
+  const responseBody = await response.json()
+  console.log(responseBody);
   expect(response.status).toEqual(200);
-
-  const responseBody = await response.json()
-})
-
-test("Teste de migrations Array", async ()=> {
-  const response = await fetch("http:/localhost:3000/api/v1/migrations");
-
-  const responseBody = await response.json()
-
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 })
+
